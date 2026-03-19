dd454e6ae197fbb9fc236f482b4c29cb6a6432b4
Shobhit Verma <shooffverm@gmail.com>
2026-03-19 15:19:42 +0530

    iybub

--- src/modules/landing-page/landing-page.controller.ts
+++ src/modules/landing-page/landing-page.controller.ts
@@ -7,6 +7,7 @@ import {
     Param,
     Delete,
     UseGuards,
+    NotFoundException,
 } from "@nestjs/common"
 import { LandingPageService } from "./landing-page.service"
 import { CreateLandingPageDto } from "./dto/create-landing-page.dto"
